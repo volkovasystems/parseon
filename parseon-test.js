e761c98180298f42ce3149318428174549b0ec51
johnlenon <johnlenonmaghanoy@gmail.com>
2017-07-13 13:30:09 +0800

    Updated server/client test.

--- parseon-test.js
+++ parseon-test.js
@@ -2,7 +2,8 @@
 const assert = require( "assert" );
 const parseon = require( "./parseon.js" );
 
-assert.ok( parseon( global ) );
+assert.equal( typeof parseon( global ), "object", "should be typeof object" );
+
 assert.deepEqual( parseon( { } ), { }, "should be equal" );
 
 console.log( "ok" );
