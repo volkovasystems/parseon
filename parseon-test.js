d5f3b150f6a6fa2b972b238f62e31b9c1235388a
johnlenon <johnlenonmaghanoy@gmail.com>
2017-07-27 20:13:48 +0800

    Updated test.

--- parseon-test.js
+++ parseon-test.js
@@ -2,8 +2,8 @@
 const assert = require( "assert" );
 const parseon = require( "./parseon.js" );
 
-assert.equal( typeof parseon( global ), "object", "should be typeof object" );
+assert.equal( typeof parseon( global ), "object", "should be equal to 'object'" );
 
-assert.deepEqual( parseon( { } ), { }, "should be equal" );
+assert.deepEqual( parseon( { "name": "bogus" } ), { "name": "bogus" }, "should be equal to { 'name': 'bogus' }" );
 
 console.log( "ok" );
