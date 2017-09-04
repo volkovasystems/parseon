65e26503ad37eb9461e36df5368baccbfc4f38af
Vinse Vinalon <vinsevinalon@gmail.com>
2017-09-04 17:05:43 +0800

    Update client/server unit test.

--- parseon-test.js
+++
@@ -1,9 +0,0 @@
-
-const assert = require( "assert" );
-const parseon = require( "./parseon.js" );
-
-assert.equal( typeof parseon( global ), "object", "should be equal to 'object'" );
-
-assert.deepEqual( parseon( { "name": "bogus" } ), { "name": "bogus" }, "should be equal to { 'name': 'bogus' }" );
-
-console.log( "ok" );
---
+++ parseon.module.js
@@ -0,0 +1,95 @@
+"use strict";
+
+/*;
+	@module-license:
+		The MIT License (MIT)
+		@mit-license
+
+		Copyright (@c) 2017 Richeve Siodina Bebedor
+		@email: richeve.bebedor@gmail.com
+
+		Permission is hereby granted, free of charge, to any person obtaining a copy
+		of this software and associated documentation files (the "Software"), to deal
+		in the Software without restriction, including without limitation the rights
+		to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+		copies of the Software, and to permit persons to whom the Software is
+		furnished to do so, subject to the following conditions:
+
+		The above copyright notice and this permission notice shall be included in all
+		copies or substantial portions of the Software.
+
+		THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+		IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+		FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+		AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+		LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+		OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+		SOFTWARE.
+	@end-module-license
+
+	@module-configuration:
+		{
+			"package": "parseon",
+			"path": "parseon/parseon.js",
+			"file": "parseon.js",
+			"module": "parseon",
+			"author": "Richeve S. Bebedor",
+			"eMail": "richeve.bebedor@gmail.com",
+			"contributors": [
+				"John Lenon Maghanoy <johnlenonmaghanoy@gmail.com>",
+				"Vinse Vinalon <vinsevinalon@gmail.com>"
+			],
+			"repository": "https://github.com:volkovasystems/parseon.git",
+			"test": "parseon-test.js",
+			"global": true
+		}
+	@end-module-configuration
+
+	@module-documentation:
+		Re-parse JSON back and forth.
+	@end-module-documentation
+
+	@include:
+		{
+			"falzy": "falzy",
+			"jnfy": "jnfy",
+			"krumb": "krumb",
+			"parsfy": "parsfy"
+		}
+	@end-include
+*/
+
+const falzy = require( "falzy" );
+const jnfy = require( "jnfy" );
+const krumb = require( "krumb" );
+const parsfy = require( "parsfy" );
+
+const parseon = function parseon( object ){
+	/*;
+		@meta-configuration:
+			{
+				"object:required": [
+					"string",
+					"object"
+				]
+			}
+		@end-meta-configuration
+	*/
+
+	if( falzy( object ) ){
+		throw new Error( "invalid object" );
+	}
+
+	try{
+		if( typeof object == "object" ){
+			return parsfy( jnfy( krumb( object ) ) );
+		}
+
+		return parsfy( object );
+
+	}catch( error ){
+		throw new Error( `cannot re-parse json object, ${ error.stack }` );
+	}
+};
+
+module.exports = parseon;
